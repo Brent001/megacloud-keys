7d1e80c186078a251d589e0959ab0a54ae950341
Grow24 <sayandas.devs@gmail.com>
2025-06-15 16:58:47 +0530

    fix

--- update_key.js
+++ update_key.js
@@ -3,8 +3,12 @@ import { exec } from "child_process";
 import { promisify } from "util";
 import axios from "axios";
 
+// const pattern = new RegExp(
+//   'var\\s+([\\w$ ,\\n\\t\\r]+);\\s+([\\w$]+)\\s*=\\s*\\[\\s*((?:(?:"[^"]*"|[^\\]"\\n\\r])+?))\\s*\\]\\s*;\\s+([\\w$]+)\\s*=\\s*\\[\\s*((?:(?:"[^"]*"|[^\\]"\\n\\r])+?))\\s*\\]\\s*;',
+//   "g"
+// );
 const pattern = new RegExp(
-  'var\\s+([\\w$ ,\\n\\t\\r]+);\\s+([\\w$]+)\\s*=\\s*\\[\\s*((?:(?:"[^"]*"|[^\\]"\\n\\r])+?))\\s*\\]\\s*;\\s+([\\w$]+)\\s*=\\s*\\[\\s*((?:(?:"[^"]*"|[^\\]"\\n\\r])+?))\\s*\\]\\s*;',
+  'var\\s+([\\w$ ,\\n\\t\\r]+);\\s+D\\s*=\\s*"--([a-fA-F0-9]+)"\\s*;',
   "g"
 );
 
@@ -14,7 +18,9 @@ const writeFileAsync = promisify(fs.writeFile);
 
 async function main() {
   try {
+    // 1. Perform axios first
     console.log("Fetching data from API.");
+    console.log(Date.now());
 
     const response = await axios.get(
       "https://megacloud.blog/js/player/a/v2/pro/embed-1.min.js?v=" + Date.now()
@@ -22,56 +28,73 @@ async function main() {
 
     console.log("Received data from API.");
 
+    // Dump into input.txt
     await writeFileAsync("input.txt", response.data, "utf8");
 
     console.log("input.txt successfully written.");
 
+    // 2. Now execute deobfuscate.js
     console.log("Running deobfuscate.js.");
 
     await execAsync("node deobfuscate.js");
 
     console.log("deobfuscate.js finished.");
 
+    // 4. Now proceed with your script
     console.log("Reading output.js.");
 
     const data = await readFileAsync("output.js", "utf8");
 
     let match;
+    match = pattern.exec(data);
+    // while ((match = pattern.exec(data)) !== null) {
+    //   const varNames = match[1]
+    //     .trim()
+    //     .split(",")
+    //     .map((v) => v.trim());
 
-    while ((match = pattern.exec(data)) !== null) {
-      const varNames = match[1]
-        .trim()
-        .split(",")
-        .map((v) => v.trim());
-      const firstArrayName = match[2].trim();
-      const firstArrayValues = match[3].trim();
+    //   const firstArrayName = match[2].trim();
+    //   const firstArrayValues = match[3].trim();
 
-      const SecondArrayName = match[4].trim();
-      const SecondArrayValues = match[5].trim();
+    //   const SecondArrayName = match[4].trim();
+    //   const SecondArrayValues = match[5].trim();
 
-      const newCode = `
-        var ${varNames.join(", ")};
+    //   // Now we can construct:
+    //   const newCode = `
+    //     var ${varNames.join(", ")};
 
-        ${firstArrayName} = [${firstArrayValues}];
-        ${SecondArrayName} = [${SecondArrayValues}];
-        let finalKey = ${SecondArrayName}.map(index => ${firstArrayName}[index]).join('');
-        return finalKey;
-      `;
-      try {
-        const finalKey = new Function(newCode)();
+    //     ${firstArrayName} = [${firstArrayValues}];
+    //     ${SecondArrayName} = [${SecondArrayValues}];
+    //     let finalKey = ${SecondArrayName}.map(index => ${firstArrayName}[index]).join('');
+    //     return finalKey;
+    //   `;
+    //   try {
+    //     const finalKey = new Function(newCode)();
 
-        if (typeof finalKey === "string") {
-          await writeFileAsync("key.txt", finalKey, "utf8");
+    //     if (typeof finalKey === "string") {
+    //       await writeFileAsync("key.txt", finalKey, "utf8");
 
-          console.log("Key successfully written.");
-        } else {
-          console.error("Generated code did not return a string.");
-        }
-      } catch (error) {
-        console.error("Error executing generated code.", error);
+    //       console.log("Key successfully written.");
+    //     } else {
+    //       console.error("Generated code did not return a string.");
+    //     }
+    //   } catch (error) {
+    //     console.error("Error executing generated code.", error);
+    //   }
+    // }
+    try {
+      const finalKey = match[2];
+
+      if (typeof finalKey === "string") {
+        await writeFileAsync("key.txt", finalKey, "utf8");
+
+        console.log("Key successfully written.");
+      } else {
+        console.error("Generated code did not return a string.");
       }
+    } catch (error) {
+      console.error("Error executing generated code.", error);
     }
-
     console.log("Done.");
   } catch (error) {
     console.error(error);
